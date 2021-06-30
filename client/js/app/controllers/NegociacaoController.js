bd78b1a7de31ac9e577964aff6665087bae25b67
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-06-30 20:05:14 -0300

    Add controller

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,25 @@
+class NegociacaoController {
+  constructor () {
+    const $ = document.querySelector.bind(document)
+    this._inputData = $('#data')
+    this._inputQuantidade = $('#quantidade')
+    this._inputValor = $('#valor')
+  }
+
+  adiciona(event) {
+    event.preventDefault()
+
+    const data = new Date(
+      ...this._inputData.value.split('-')
+        .map((v, i) => v - i % 2)
+    )
+
+    const negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value,
+    )
+
+    console.log(negociacao)
+  }
+}
