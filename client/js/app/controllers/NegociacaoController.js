d2029a793f40dd18e9a96a762dcceaf12df624fe
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-07-01 17:04:33 -0300

    Class to deal with dates and imutable list of negotiaitons

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,22 +4,33 @@ class NegociacaoController {
     this._inputData = $('#data')
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
+    this._listaNegociacoes = new ListaNegociacoes()
   }
 
   adiciona(event) {
     event.preventDefault()
 
-    const data = new Date(
-      ...this._inputData.value.split('-')
-        .map((v, i) => v - i % 2)
-    )
+    this._listaNegociacoes
+      .adiciona(this._criaNegociacao())
+    this._limpaFormulario()
 
-    const negociacao = new Negociacao(
-      data,
+    console.log(this._listaNegociacoes.negociacoes)
+
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value,
     )
+  }
 
-    console.log(negociacao)
+  _limpaFormulario() {
+    this._inputData.value = ''
+    this._inputQuantidade.value = 1
+    this._inputValor.value = 0
+
+    this._inputData.focus()
   }
 }
