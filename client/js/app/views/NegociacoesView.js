2d59659a0b9b30b8138f48c6a648a2590f2ca55b
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-07-02 15:03:03 -0300

    Add view

---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,50 @@
+class NegociacoesView {
+  constructor(elemento) {
+    this._elemento = elemento
+  }
+
+  _template(model) {
+    return `
+      <table class="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th>DATA</th>
+            <th>QUANTIDADE</th>
+            <th>VALOR</th>
+            <th>VOLUME</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          ${
+            model.negociacoes.map((v) => `
+              <tr>
+                <td>${DateHelper.dataParaTexto(v.data)}</td>
+                <td>${v.quantidade}</td>
+                <td>${v.valor}</td>
+                <td>${v.volume}</td>
+              </tr>
+            `)
+            .join('')
+          }
+        </tbody>
+
+        <tfoot>
+          <td colspan="3"></td>
+          <td>
+            ${
+              model.negociacoes
+                .reduce((prev, curr) => prev + curr.volume, 0.0)
+            }
+          </td>
+        </tfoot>
+      </table>
+    `
+  }
+
+  update(model) {
+    this._elemento.innerHTML = this._template(model)
+  }
+}
+/*
+*/
