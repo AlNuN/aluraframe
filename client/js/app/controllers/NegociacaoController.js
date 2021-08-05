b2584d7c07650fc217961cc403ff14c1e59df7a6
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-08-05 20:54:05 -0300

    Create DAO to access the indexedDB

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -18,16 +18,36 @@ class NegociacaoController {
       new MensagemView($('#mensagemView')),
       'texto'
     )
+
+    ConnectionFactory
+      .getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.listaTodos())
+      .then(negociacoes => 
+        negociacoes.forEach(negociacao => 
+          this._listaNegociacoes.adiciona(negociacao)))
+      .catch(error => {
+        console.log(error)
+        this._mensagem.texto = error
+      })
   }
 
   adiciona(event) {
     event.preventDefault()
 
-    this._listaNegociacoes.adiciona(this._criaNegociacao())
-
-    this._mensagem.texto = 'Negociação adicionada com sucesso'
-
-    this._limpaFormulario()
+    ConnectionFactory
+      .getConnection()
+      .then(connection => {
+        let negociacao = this._criaNegociacao()
+        new NegociacaoDao(connection)
+          .adiciona(negociacao)
+          .then(() => {
+            this._listaNegociacoes.adiciona(negociacao)
+            this._mensagem.texto = 'Negociação adicionada com sucesso'
+            this._limpaFormulario()
+          })
+      })
+      .catch(error => this._mensagem.texto = error)
   }
 
   importaNegociacoes() {
@@ -41,16 +61,25 @@ class NegociacaoController {
   }
 
   apaga() {
-    this._listaNegociacoes.esvazia()
-
-    this._mensagem.texto = 'Negociações apagadas com sucesso'
+    ConnectionFactory
+      .getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.apagaTodos())
+      .then(mensagem => {
+        this._mensagem.texto = mensagem
+        this._listaNegociacoes.esvazia()
+      })
+      .catch(e => {
+        this._mensagem.texto = e
+        console.log(e)
+      })
   }
 
   _criaNegociacao() {
     return new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
-      this._inputQuantidade.value,
-      this._inputValor.value,
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value),
     )
   }
 
